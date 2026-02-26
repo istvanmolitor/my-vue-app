2c8790456b02d9d314964bb44e5087367b44b124
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-02-26 11:25:03 +0100

    Register Admin menu

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { menuRegistry } from './packages/ts-menu'
+import { AdminMenuBuilder } from './packages/vue-admin'
+
+menuRegistry.register(new AdminMenuBuilder())
 
 createApp(App).use(router).mount('#app')
